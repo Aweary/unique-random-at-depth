d2e7916d3c0f55e5efa2ec4e1c8b8784dec19861
Brandon Dail <Kierkegaurd@gmail.com>
2016-03-06 15:49:42 -0600

    Implement optional depth argument

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 'use strict';
-module.exports = function (min, max) {
-	var prev;
+module.exports = function (min, max, depth) {
+	var depth = depth && depth > 0 ? depth : 1;
+	var range = max - min + 1;
+	if (range < depth) {
+		throw new Error(
+			'The depth argument must be greater than ' +
+			'the total number of allowable values.'
+		);
+	}
+	var previous = [];
+
+	if (min === max) return function() { return min };
+
 	return function rand() {
-		var num = Math.floor(Math.random() * (max - min + 1) + min);
-		return prev = num === prev && min !== max ? rand() : num;
+		var num = Math.floor(Math.random() * range + min);
+		if (previous.indexOf(num) !== -1) {
+			num = rand();
+		}
+		else {
+			previous.unshift(num);
+			if (previous.length > depth) previous.pop();
+		}
+		return num;
 	};
 };
--- test.js
+++ test.js
@@ -1,15 +1,17 @@
 'use strict';
 var assert = require('assert');
-var uniqueRandom = require('./')(1, 10);
+var uniq = require('lodash.uniq');
+var uniqueRandomAtDepth = require('./');
 
 it('should generate a random number from min to max without the same number in a row', function () {
-	var count = 1000;
+	var random = uniqueRandomAtDepth(1, 10);
 	var fail = false;
 	var current;
+	var count = 1000;
 	var prev;
 
 	while (--count > 0) {
-		current = uniqueRandom();
+		current = random();
 
 		if (current === prev || current > 10 || current < 1) {
 			fail = true;
@@ -20,3 +22,41 @@ it('should generate a random number from min to max without the same number in a
 
 	assert(!fail);
 });
+
+it('should respect the depth argument', function() {
+
+	var depth = 5;
+	var random = uniqueRandomAtDepth(1, 10, depth);
+	var previous = [];
+	var fail = false;
+	var count = 1000;
+	var current;
+
+	while(--count > 0) {
+	  var current = random();
+		previous.unshift(current);
+		if (previous.length > depth) previous.pop();
+ 		if (uniq(previous).length !== previous.length) {
+			fail = true;
+		}
+	}
+
+	assert(!fail);
+
+})
+
+
+it('should return the same value everytime if min and max are equal',
+function() {
+	var count = 1000;
+	var random = uniqueRandomAtDepth(1, 1);
+	var expected = 1;
+	var actual = 1;
+	var fail = false;
+
+	while (--count > 0) {
+		actual = random();
+		if (actual !== expected) fail = true;
+	}
+	assert(!fail);
+})
